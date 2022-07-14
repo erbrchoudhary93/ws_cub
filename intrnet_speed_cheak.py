d161b4ad769837b25a2219fee6d12e55ddefd071
erbrchoudhary <erbrchoudhary0789@gmail.com>
2022-07-14 20:22:29 +0530

    cheak

--- intrnet_speed_cheak.py
+++ intrnet_speed_cheak.py
@@ -22,7 +22,7 @@ lab = Label(sp,text="Download Speed ",font=("Time New Roman",30,"bold"))
 lab.place(x=60,y=130,height=50,width=380)
 
 lab_down = Label(sp,text="00",font=("Time New Roman",30,"bold"))
-lab_down.place(x=60,y=200,height=50,width=380)
+lab_down.place(x=60,y=200,height=50,width=380) # ram
 
 lab = Label(sp,text="Upload speed",font=("Time New Roman",30,"bold"))
 lab.place(x=60,y=290,height=50,width=380)
